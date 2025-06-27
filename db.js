4fe842ff918bd5e072eefcc0dd7da1dfd4193562
marant7 <mf2018000597@virtual.upt.pe>
2025-06-27 14:19:21 -0500

    terminado

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '161.132.53.196',
+  user: 'testuser',
+  password: 'testpass',
   database: 'testdb',
   waitForConnections: true,
   connectionLimit: 10,
